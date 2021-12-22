b986fdca1d4605de681e288ef5fc0eb2ed575235
Ondrejmuran4691 <58992354+Ondrejmuran4691@users.noreply.github.com>
2021-12-22 20:26:33 +0100

    category is still possible to read

---
+++ openlab 10.05/openlab 10.05/Book.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace openlab_10._05
+{
+    public class Book
+    {
+        /// <summary>
+        /// static list with book categories
+        /// </summary>
+        public static string[] categoryList = new string[] { "detske", "romanticke", "naučne", "sci-fi", "dobrodružné" };
+       
+
+        
+        public Book()
+        {
+            Title = "-1";
+            ReleaseDate = -1;
+            Author = "-1";
+            Pages = -1;
+            Category = "-1";
+        }
+
+        public Book(string titleofbook, int pagesofbook)
+        {
+            Title = titleofbook;
+            ReleaseDate = -1;
+            Author = "-1";
+            Pages = pagesofbook;
+            Category = "-1";
+        }
+        public Book(string titleofbook, string authorofbook, string categoryofbook, int pagesofbook, int releasedateofbook)
+        {
+            Title = titleofbook;
+            ReleaseDate = releasedateofbook;
+            Author = authorofbook;
+            Pages = pagesofbook;
+            
+        }
+        public string Title { set; get; }
+
+        private string _category;  
+        public string Category    
+        {
+            get => _category;
+            set => _category = value;
+        }
+        private int pages;
+        public int Pages
+        {
+
+            get
+            {
+                return pages;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    value = 1;
+                }
+                pages = value;
+            }
+
+        }
+
+        
+        public string Author { set; get; }
+
+        private int releaseDate;
+        public int ReleaseDate
+        {
+
+            get
+            {
+                return releaseDate;
+            }
+            set
+            {
+                if (value < 1405)
+                {
+                    value = -1;
+                }
+                else
+                {
+                    if (value > 2021)
+                    {
+                        value = -1;
+                    }
+                }
+                releaseDate = value;
+            }
+
+        }
+
+
+
+        public void writeall()
+        {
+
+            Console.WriteLine(Title);
+            Console.WriteLine(Pages);
+           
+            Console.WriteLine(Author);
+            Console.WriteLine(ReleaseDate);
+        }
+
+
+
+
+
+    }
+}
---
+++ openlab 10.05/openlab 10.05/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace openlab_10._05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Book LOTR = new Book();
+
+
+            LOTR.Title = "LOTR";
+            LOTR.Author = "J. R. R. Tolkien";
+            LOTR.Category = "Fantasy";
+            LOTR.Pages = 9250;
+            LOTR.ReleaseDate = 1954;
+
+            Book LOTR2 = new Book();
+            Book LOTR3 = new Book("LOTR3", 156);
+            Book HOBIT = new Book("HOBIT", "J. R. R. Tolkien", "Fantasy", 6544, 5);
+            HOBIT.Category = Book.categoryList[4];
+            LOTR.writeall();
+            LOTR2.writeall();
+            LOTR3.writeall();
+            HOBIT.writeall();
+            Console.WriteLine(HOBIT.Category);
+            
+            
+        }
+    }
+
+}
